04700641415663699baee3d9dbd5ff5ed5254481
Jason <chong-1997@hotmail.com>
2017-12-09 19:21:14 +0800

    First commit

---
+++ second_project/blog/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+import datetime
+from django import template
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Hello Django")
+
+def today_is(request):
+	now = datetime.datetime.now()
+	return render(request, 'blog/datetime.html', {'now': now})
